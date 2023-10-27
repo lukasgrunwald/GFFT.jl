022fa667216b88afe0425b795a105d116526700b
Documenter.jl <documenter@juliadocs.github.io>
2023-10-27 15:01:18 +0000

    build based on b29fad5

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = GFFT","category":"page"},{"location":"#GFFT","page":"Home","title":"GFFT","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for GFFT.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [GFFT]","category":"page"}]
+}
